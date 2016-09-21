a334ae77cf297929b3a31cb1d13e72ac0e2b4bf9
Sathish Kumar <sathishvskumar@gmail.com>
2016-09-21 05:56:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.send('Article one');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
