9abb81efbc779df8e6907d5507e38f2cc313b9dc
Sathish Kumar <sathishvskumar@gmail.com>
2016-10-01 20:23:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='new value';  
+var moveRight=0;
+function moveRight()
+{
+     moveRight=moveRight+5;
+    img.style.marginRight =moveRight+'px'; 
+}
+var img = document.getElementById('madi');
+img.setInterval(moveRight,50);
