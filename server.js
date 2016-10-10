92ec11b61d49254a041d96801358be3e5c0edf57
Sathish Kumar <sathishvskumar@gmail.com>
2016-10-10 08:26:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/ui/main.js',function(req,res){
     
 });
 var names=[];
-app.get('/submit-name/name?=',function(req,res)
+app.get('/submit-name?name=',function(req,res)
 {
     var name=req.query.name;
     names.push(name);
