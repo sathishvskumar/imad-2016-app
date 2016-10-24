466709e0f8ef2d82981b719cc0c65eba9db39fe2
Sathish Kumar <sathishvskumar@gmail.com>
2016-10-24 18:17:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 var config={
     user:'sathishvskumar',
     database:'sathishvskumar',
-    host:'db.imad.hasura-app.io',
+    host:'localhost',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
