1a9d7b20134e8bfb6b6e6096406ddb3204914c25
Sathish Kumar <sathishvskumar@gmail.com>
2016-10-25 13:18:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var Pool = require('pg').Pool;
 //host:'db.imad.hasura-app.io',
 var config={
     host:'db.imad.hasura-app.io',
-    port:5432,
+    port:'5432',
     database:'sathishvskumar',
     user:'sathishvskumar',
-    password:process.env.DB_PASSWORD
+    password:'db-sathishvskumar-47722'
 };
 
 var app = express();
