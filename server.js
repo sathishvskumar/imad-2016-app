4cf31188233d3b774fefe5632d1a791d6c56c917
Sathish Kumar <sathishvskumar@gmail.com>
2016-11-12 08:00:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config={
     user:'sathishvskumar',
@@ -70,6 +71,18 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+//Implement Hash 
+
+function hash(input,salt)
+{
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512') ;
+}
+
+app.get('/hash/:input',function(){
+   var hashString = hash(req.params.input,"randomstring" 
+   res.send(hashString);
 });
 
 var pool = new Pool(config);
